8748039babe35d908a6b7b2f347715a3c867fafb
oznurtopcu <topcuoznur121@gmail.com>
2024-12-29 23:42:47 +0300

    import sampleData in orderForm

--- src/components/OrderForm.jsx
+++ src/components/OrderForm.jsx
@@ -1,12 +1,14 @@
 import {Form, FormGroup, Label, Input, Button} from 'reactstrap';
+import sampleData from '../sampleData';
 export default function OrderForm() {
+    const {baslik, fiyat, puan, yorumSayisi, aciklama, ekstraMalzeme} = sampleData;
     return (
         <Form>
-            <h3>Position Absolute Acı Pizza</h3>
-            <p>85.50₺</p>
-            <p>4.9</p>
-            <p>(200)</p>
-            <p>Frontent Dev olarak hala position:absolute kullanıyorsan bu çok acı pizza tam sana göre. Pizza, domates, peynir ve genellikle çeşitli diğer malzemelerle kaplanmış, daha sonra geleneksel olarak odun ateşinde bir fırında yüksek sıcaklıkta pişirilen, genellikle yuvarlak, düzleştirilmiş mayalı buğday bazlı hamurdan oluşan İtalyan kökenli lezzetli bir yemektir. . Küçük bir pizzaya bazen pizzetta denir.</p>
+            <h3>{baslik}</h3>
+            <p>{fiyat}₺</p>
+            <p>{puan}</p>
+            <p>({yorumSayisi})</p>
+            <p>{aciklama}</p>
             <FormGroup>
                 <FormGroup tag="fieldset">
                     <legend>
@@ -71,29 +73,16 @@ export default function OrderForm() {
                <p>En fazla 10 malzeme seçebilirsiniz.</p>
             </div>
             <FormGroup>
-                <FormGroup check>
-                    <Input type="checkbox" />
-                    {' '}
-                    <Label check>
-                    Pepperoni
-                    </Label>
-                </FormGroup>
-                <FormGroup check>
-                    <Input type="checkbox" />
-                    {' '}
-                    <Label check>
-                    Ton balığı
-                    </Label>
-                </FormGroup>
-                <FormGroup check>
-                    <Input type="checkbox" />
-                    {' '}
-                    <Label check>
-                    Mısır
-                    </Label>
-                </FormGroup>
+                {ekstraMalzeme.map((malzeme,index) => {
+                    return (<FormGroup key={index} check>
+                        <Input type="checkbox"/>
+                        {' '}
+                        <Label check>
+                        {malzeme}
+                        </Label>
+                    </FormGroup>)
+                })}
             </FormGroup>
-            
             <FormGroup>
                 <Label for="isim">
                 İsim
--- src/sampleData.js
+++ src/sampleData.js
@@ -1,10 +1,10 @@
 const sampleData = {
-baslik: "Position Absolute Acı Pizza",
-fiyat: "85.50",
+baslik: "Position Absolute Pizza",
+fiyat: "98.50",
 puan: "4.9",
 yorumSayisi: "200",
 aciklama: "Frontent Dev olarak hala position:absolute kullanıyorsan bu çok acı pizza tam sana göre. Pizza, domates, peynir ve genellikle çeşitli diğer malzemelerle kaplanmış, daha sonra geleneksel olarak odun ateşinde bir fırında yüksek sıcaklıkta pişirilen, genellikle yuvarlak, düzleştirilmiş mayalı buğday bazlı hamurdan oluşan İtalyan kökenli lezzetli bir yemektir. . Küçük bir pizzaya bazen pizzetta denir.",
-boyut: "",
-kalinlik:"",
 ekstraMalzeme: ["Pepperoni", "Sosis", "Kanada Jambonu", "Tavuk Izgara", "Soğan", "Domates", "Mısır", "Sucuk", "Jalapeno", "Sarımsak", "Biber", "Ananas", "Kabak"],
 };
+
+export default sampleData;
