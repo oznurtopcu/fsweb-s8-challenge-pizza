4fd3e9265d0380b01aa2a1c7e5a03b479195ad61
oznurtopcu <topcuoznur121@gmail.com>
2024-12-31 17:06:15 +0300

    import components in orderForm.jsx
    The newly created components were imported into orderForm.jsx.

--- src/components/OrderPizza/OrderForm.jsx
+++ src/components/OrderPizza/OrderForm.jsx
@@ -12,8 +12,30 @@ import ProductInfo from './ProductInfo/ProductInfo';
 export default function OrderForm() {
     return (
         <div className='form-content'>
+            <Form onSubmit={handleSubmit}>
+                {/*ürün bilgisi bölümü*/}
+                <ProductInfo sampleData={sampleData}/>
+                
+                <div className='select-content'>
+                    {/*pizza boyutu seçim bölümü*/}
+                    <SizeSelector data={data} setData={setData}/>
+                    
+                    {/*hamur kalınlığı seçim bölümü*/}
+                    <ThicknessSelector data={data} setData={setData}/>
                 </div>
+                
+                {/*ekstra malzeme seçim bölümü*/}
+                <ToppingSelector ekstraMalzeme={ekstraMalzeme} data={data} setData={setData} />
+                
+                {/*isim ve sipariş notu input bölümü*/}
+                <InputField data={data} setData={setData}/>
 
+                <hr/>
+
+                <div className='order-card'>
+                    <Summary data={data} setData={setData} secimler={secimler} miktar={miktar} setMiktar={setMiktar} toplam={toplam} isValid={isValid}/>
+                </div>
+            </Form>
         </div>
         
     );
