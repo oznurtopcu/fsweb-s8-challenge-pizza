1fc23922b8097a5d907ef483223fd4914bfdd051
oznurtopcu <topcuoznur121@gmail.com>
2025-01-02 22:13:52 +0300

    update success.jsx

--- src/components/Success/Success.jsx
+++ src/components/Success/Success.jsx
@@ -1,16 +1,39 @@
 import logo from '../../../images/iteration-1-images/logo.svg';
 import './Success.css';
-export default function Success() {
+export default function Success(props) {
+    const {pageRouter, dataResponse} = props;
+    console.log(dataResponse);
 return (
-    <div className='success-content'>
+    <div className='success-info'>
         <div className='success-card'>
-            <img src={logo} alt="Logo" className="logo"/>
             <div className="success-text">
-                <p>TEBRİKLER!</p>
-                <p>SİPARİŞİNİZ ALINDI!</p>
+                <p id="lezzet">lezzetin yolda</p>
+                <p>SİPARİŞ ALINDI</p>
             </div>
+
+            <hr/>
+
+            <div className='order'>
+                <h4>Acı Pizza</h4>
+                <div className='order-info'>
+                    <p>Boyut: <span>{dataResponse.boyut}</span></p>
+                    <p>Hamur: <span>{dataResponse.kalinlik}</span></p>
+                    <p>Ek Malzemeler: <span>{dataResponse.extra.join()}</span></p>
+                </div>
+            </div>
+            
+            <div className='order-summary'>
+                <p>Sipariş Toplamı</p>
+                    <div id='addition'>
+                        <p>Seçimler</p>
+                        <p>20.00₺</p>
+                    </div>
+                    <div id='addition'>
+                        <p>Toplam</p>
+                        <p>105.50₺</p>
+                    </div>
+            </div> 
+            
         </div>
     </div>
-
-
 )}
