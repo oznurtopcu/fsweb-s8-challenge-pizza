96fc759428ddeac8223f02c1d10dd892a18177aa
oznurtopcu <topcuoznur121@gmail.com>
2025-01-03 11:55:38 +0300

    add NavMenu

---
+++ src/components/Home/NavMenu/NavMenu.jsx
@@ -0,0 +1,14 @@
+import './NavMenu.css';
+
+export default function NavMenu() {
+    return(
+        <nav>
+            <a href="#"><img src="../../../../images/iteration-2-images/icons/1.svg" alt="Yeni Kore" />YENİ! Kore</a>
+            <a href="#"><img src="../../../../images/iteration-2-images/icons/2.svg" alt="Yeni Kore" />Pizza</a>
+            <a href="#"><img src="../../../../images/iteration-2-images/icons/3.svg" alt="Yeni Kore" />Burger</a>
+            <a href="#"><img src="../../../../images/iteration-2-images/icons/4.svg" alt="Yeni Kore" />Kızartmalar</a>
+            <a href="#"><img src="../../../../images/iteration-2-images/icons/5.svg" alt="Yeni Kore" />Fast Food</a>
+            <a href="#"><img src="../../../../images/iteration-2-images/icons/6.svg" alt="Yeni Kore" />Gazlı İçecek</a>
+        </nav>
+    );
+}
