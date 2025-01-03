0033f1074d9f724955c6315ad929363fd62858c0
oznurtopcu <topcuoznur121@gmail.com>
2025-01-03 23:20:36 +0300

    add NavMenu in Home

--- src/components/Home/NavMenu/NavMenu.jsx
+++ src/components/Home/NavMenu/NavMenu.jsx
@@ -1,14 +1,12 @@
 import './NavMenu.css';
 
-export default function NavMenu() {
+export default function NavMenu(props) {
+    const {menuList} = props;
     return(
         <nav>
-            <a href="#"><img src="../../../../images/iteration-2-images/icons/1.svg" alt="Yeni Kore" />YENİ! Kore</a>
-            <a href="#"><img src="../../../../images/iteration-2-images/icons/2.svg" alt="Yeni Kore" />Pizza</a>
-            <a href="#"><img src="../../../../images/iteration-2-images/icons/3.svg" alt="Yeni Kore" />Burger</a>
-            <a href="#"><img src="../../../../images/iteration-2-images/icons/4.svg" alt="Yeni Kore" />Kızartmalar</a>
-            <a href="#"><img src="../../../../images/iteration-2-images/icons/5.svg" alt="Yeni Kore" />Fast Food</a>
-            <a href="#"><img src="../../../../images/iteration-2-images/icons/6.svg" alt="Yeni Kore" />Gazlı İçecek</a>
+            {menuList.map((item,index) => {
+                return <a href="#"><img src={`../../../../images/iteration-2-images/icons/${index+1}.svg`}  alt={item} />{item}</a>
+            })}
         </nav>
     );
 }
