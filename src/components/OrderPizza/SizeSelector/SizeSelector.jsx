5e53538a070b0b96976d0d910705f689bf3cff5f
oznurtopcu <topcuoznur121@gmail.com>
2025-01-06 02:37:40 +0300

    use map function in sizeSelector

--- src/components/OrderPizza/SizeSelector/SizeSelector.jsx
+++ src/components/OrderPizza/SizeSelector/SizeSelector.jsx
@@ -11,6 +11,32 @@ export default function SizeSelector(props) {
                         <Label className='label'>
                             Boyut seç*
                         </Label>
+                        {size.map((item, index) => {
+                            return (<FormGroup>
+                                <Input
+                                    name="boyut"
+                                    type="radio"
+                                    id={item}
+                                    value={item}
+                                    onClick={handleChange}
+                                />
+                                <Label htmlFor={item}>
+                                    {item}
+                                </Label>
+                            </FormGroup>)
+                            
+                        })}
+        </FormGroup>        
+
+    );
+}
+
+{
+    /*
+
+                            <Label className='label'>
+                            Boyut seç*
+                        </Label>
                         <FormGroup>
                             <Input
                                 name="boyut"
@@ -46,8 +72,79 @@ export default function SizeSelector(props) {
                             <Label htmlFor='large'>
                                 Büyük
                             </Label>
-                        </FormGroup>    
-                    </FormGroup>        
+                        </FormGroup>
+    
 
-    );
+
+
+
+
+                            <div className='radio-group'>
+                            <label className="container"> 
+                                <input type="radio" name="boyut" value="S" onClick={handleChange}/>
+                                <span className="checkmark">S</span>
+                            </label>
+                            <label className="container"> 
+                                <input type="radio" name="boyut" value="M" onClick={handleChange}/>
+                                <span className="checkmark">M</span>
+                            </label>
+                            <label className="container"> 
+                                <input type="radio" name="boyut" value="L" onClick={handleChange}/>
+                                <span className="checkmark">L</span>
+                            </label>
+                        </div>
+
+
+
+
+
+
+
+
+
+.radio-group {
+    display: flex;
+    flex-direction: row;
+    gap: 2rem;
+    align-items: center;
+    margin: auto;
+}
+
+.container {
+    display: flex;
+    cursor: pointer;
+    font-size: 1rem;
+    font-weight: bold;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+  }
+  
+
+  .container input {
+    height: 2rem;
+    width: 2rem;
+    opacity: 0;
+    cursor: pointer;
+  }
+  .checkmark {
+    position: absolute;
+    height: 2.5rem;
+    width: 2.5rem;
+    background-color: #eee;
+    border-radius: 50%;
+    text-align: center;
+  }
+  
+  .container:hover input ~ .checkmark {
+    background-color: #ccc;
+  }
+  
+  .container input:checked ~ .checkmark {
+    background-color: var(--primary-yellow);
+  }
+    
+    
+    */
 }
