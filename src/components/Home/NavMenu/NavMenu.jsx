c5c2e89491547b5ba2ff9ca280c9edd18c893ffa
oznurtopcu <topcuoznur121@gmail.com>
2025-01-06 02:50:36 +0300

    add key in navmenu

--- src/components/Home/NavMenu/NavMenu.jsx
+++ src/components/Home/NavMenu/NavMenu.jsx
@@ -5,7 +5,7 @@ export default function NavMenu(props) {
     return(
         <nav>
             {menuList.map((item,index) => {
-                return <a href="#"><img src={`../../../../images/iteration-2-images/icons/${index+1}.svg`}  alt={item} />{item}</a>
+                return <a href="#" key={index} ><img src={`../../../../images/iteration-2-images/icons/${index+1}.svg`}  alt={item} />{item}</a>
             })}
         </nav>
     );
